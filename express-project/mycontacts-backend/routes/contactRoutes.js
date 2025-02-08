f72dde64584cb6d2b27889c22d3c4b67fa62d6ce
devishi-r <129303782+devishi-r@users.noreply.github.com>
2025-02-08 11:13:34 +0530

    Router

--- express-project/mycontacts-backend/routes/contactRoutes.js
+++ express-project/mycontacts-backend/routes/contactRoutes.js
@@ -1,2 +1,28 @@
 const express = require("express");
 const router = express.Router();
+
+//configuring routes on our router 'router'
+router.route('/').get((request, response) => {
+    response.status(200).json({message: "Get all contacts" });
+})
+
+//specifying PUT, POST, DELETE: 
+router.route('/').post((request, response) => {
+    response.status(200).json({message: "Create contact" });
+})
+
+router.route('/:id').put((request, response) => {
+    response.status(200).json({message: `Modify contact: ${request.params.id}` });
+})
+
+router.route('/:id').delete((request, response) => {
+    response.status(200).json({message: `Delete contact: ${request.params.id}` });
+})
+
+//get individual contact
+router.route('/:id').get((request, response) => {
+    response.status(200).json({message: `Fetch contact: ${request.params.id}` });
+})
+
+
+module.exports = router;
--- express-project/mycontacts-backend/server.js
+++ express-project/mycontacts-backend/server.js
@@ -6,9 +6,12 @@ const port = process.env.PORT || 5000;
 
 //this is great, but we're not going to configure all our routes in server.js
 //hence -> new folder to handle all our routes: routes :)
-app.get('/api/contacts', (request, response) => {
-    response.status(200).json({message: "Get all contacts" });
-});
+// app.get('/api/contacts', (request, response) => {
+//     response.status(200).json({message: "Get all contacts" });
+// });
+
+//replaced get with use- middleware
+app.use('/api/contacts', require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
